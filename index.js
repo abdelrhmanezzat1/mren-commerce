d58b7de31a5c16db3cefd662b44c033b457d4060
abdoezzat <abdelrhmansoliman101@gmail.com>
2022-07-02 00:14:19 +0200

    ready

--- index.js
+++ index.js
@@ -24,8 +24,7 @@ app.use("/api/prodects", prodect);
 app.use("/api/cart", Cart);
 app.use("/api/order", Order);
 app.use("/api/check", stripe);
-app.use(express.static("client/build"));
-app.get("*", (req, res) => {
-    res.sendFile(`${__dirname}/client/build/index.html`);
-});
+app.use(express.static(path.resolve(__dirname, "./client/build")))
+app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "./client/build", "index.html")))
+
 app.listen(process.env.PORT || Port, () => console.log(`server running in port ${Port}...`))
